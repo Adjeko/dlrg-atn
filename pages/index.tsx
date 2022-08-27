e5806b071bf754edb95e6e6d4831bce395802ec2
Adjeko <adjeko88@gmail.com>
2022-08-27 11:05:35 +0200

    added stats and timeline to profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,165 @@
 import AppShell from "../components/appshell"
 import type { NextPage } from 'next'
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
+import { CheckIcon, HandThumbUpIcon, UserIcon } from '@heroicons/react/20/solid'
+
+const stats = [
+  { name: 'Total Subscribers', stat: '71,897', previousStat: '70,946', change: '12%', changeType: 'increase' },
+  { name: 'Avg. Open Rate', stat: '58.16%', previousStat: '56.14%', change: '2.02%', changeType: 'increase' },
+  // { name: 'Avg. Click Rate', stat: '24.57%', previousStat: '28.62%', change: '4.05%', changeType: 'decrease' },
+]
+
+const timeline = [
+  {
+    id: 1,
+    content: 'Applied to',
+    target: 'Front End Developer',
+    href: '#',
+    date: 'Sep 20',
+    datetime: '2020-09-20',
+    icon: UserIcon,
+    iconBackground: 'bg-gray-400',
+  },
+  {
+    id: 2,
+    content: 'Advanced to phone screening by',
+    target: 'Bethany Blake',
+    href: '#',
+    date: 'Sep 22',
+    datetime: '2020-09-22',
+    icon: HandThumbUpIcon,
+    iconBackground: 'bg-blue-500',
+  },
+  {
+    id: 3,
+    content: 'Completed phone screening with',
+    target: 'Martha Gardner',
+    href: '#',
+    date: 'Sep 28',
+    datetime: '2020-09-28',
+    icon: CheckIcon,
+    iconBackground: 'bg-green-500',
+  },
+  {
+    id: 4,
+    content: 'Advanced to interview by',
+    target: 'Bethany Blake',
+    href: '#',
+    date: 'Sep 30',
+    datetime: '2020-09-30',
+    icon: HandThumbUpIcon,
+    iconBackground: 'bg-blue-500',
+  },
+  {
+    id: 5,
+    content: 'Completed interview with',
+    target: 'Katherine Snyder',
+    href: '#',
+    date: 'Oct 4',
+    datetime: '2020-10-04',
+    icon: CheckIcon,
+    iconBackground: 'bg-green-500',
+  },
+]
+
+//TODO type anlegen
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Home: NextPage = () => {
   return (
     <AppShell>
-      <h1> Dein Profil</h1>
+      <div>
+        {/* Statistiken */}
+        <h3 className="text-lg font-medium leading-6 text-gray-900">Last 30 days</h3>
+        <dl className="grid grid-cols-1 mt-5 overflow-hidden bg-white divide-y divide-gray-200 rounded-lg shadow md:grid-cols-2 md:divide-y-0 md:divide-x">
+          {stats.map((item) => (
+            <div key={item.name} className="px-4 py-5 sm:p-6">
+              <dt className="text-base font-normal text-gray-900">{item.name}</dt>
+              <dd className="flex items-baseline justify-between mt-1 md:block lg:flex">
+                <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
+                  {item.stat}
+                  <span className="ml-2 text-sm font-medium text-gray-500">from {item.previousStat}</span>
+                </div>
+
+                <div
+                  className={classNames(
+                    item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800',
+                    'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0'
+                  )}
+                >
+                  {item.changeType === 'increase' ? (
+                    <ArrowUpIcon
+                      className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <ArrowDownIcon
+                      className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500"
+                      aria-hidden="true"
+                    />
+                  )}
+
+                  <span className="sr-only">{item.changeType === 'increase' ? 'Increased' : 'Decreased'} by</span>
+                  {item.change}
+                </div>
+              </dd>
+            </div>
+          ))}
+        </dl>
+
+        {/* Divider */}
+        <div className="relative mt-10 mb-6">
+          <div className="absolute inset-0 flex items-center" aria-hidden="true">
+            <div className="w-full border-t border-gray-300" />
+          </div>
+          <div className="relative flex justify-center">
+            <span className="px-3 text-lg font-medium text-gray-900 bg-white">Timeline</span>
+          </div>
+        </div>
+
+        {/* Timeline */}
+        <div className="flow-root">
+          <ul role="list" className="-mb-8">
+            {timeline.map((event, eventIdx) => (
+              <li key={event.id}>
+                <div className="relative pb-8">
+                  {eventIdx !== timeline.length - 1 ? (
+                    <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                  ) : null}
+                  <div className="relative flex space-x-3">
+                    <div>
+                      <span
+                        className={classNames(
+                          event.iconBackground,
+                          'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
+                        )}
+                      >
+                        <event.icon className="w-5 h-5 text-white" aria-hidden="true" />
+                      </span>
+                    </div>
+                    <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                      <div>
+                        <p className="text-sm text-gray-500">
+                          {event.content}{' '}
+                          <a href={event.href} className="font-medium text-gray-900">
+                            {event.target}
+                          </a>
+                        </p>
+                      </div>
+                      <div className="text-sm text-right text-gray-500 whitespace-nowrap">
+                        <time dateTime={event.datetime}>{event.date}</time>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+      </div>
     </AppShell>
   )
 }
