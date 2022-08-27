695192abef11bdc9a009d45bfbeb3907d728dd7e
Adjeko <adjeko88@gmail.com>
2022-08-27 14:17:30 +0200

    populated course page

--- pages/course.tsx
+++ pages/course.tsx
@@ -2,7 +2,29 @@ import AppShell from "../components/appshell"
 import { PlusIcon as PlusIconOutline } from '@heroicons/react/24/outline'
 import React, { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { PaperClipIcon } from '@heroicons/react/20/solid'
 import QRCode from "react-qr-code";
+
+const people = [
+	{
+		name: 'Calvin Hawkins',
+		email: 'calvin.hawkins@example.com',
+		image:
+			'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+	},
+	{
+		name: 'Kristen Ramos',
+		email: 'kristen.ramos@example.com',
+		image:
+			'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+	},
+	{
+		name: 'Ted Fox',
+		email: 'ted.fox@example.com',
+		image:
+			'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+	},
+]
 
 const Course = () => {
 	const [open, setOpen] = useState(false)
@@ -17,6 +39,111 @@ const Course = () => {
 
 	return (
 		<AppShell>
+
+			{/* Kursbeschreibung */}
+			<>
+				<div>
+					<h3 className="text-2xl font-medium leading-6 text-gray-900">Sanitätskurs A</h3>
+				</div>
+				<div className="mt-5 border-t border-gray-200">
+					<dl className="divide-y divide-gray-200">
+						<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+							<dt className="text-sm font-medium text-gray-500">Kursname</dt>
+							<dd className="flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow">Sanitätskurs A</span>
+								<span className="flex-shrink-0 ml-4">
+									<button
+										type="button"
+										className="font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Update
+									</button>
+								</span>
+							</dd>
+						</div>
+						<div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+							<dt className="text-sm font-medium text-gray-500">Veranstalter</dt>
+							<dd className="flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow">Timo Imhof</span>
+								<span className="flex-shrink-0 ml-4">
+									<button
+										type="button"
+										className="font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Update
+									</button>
+								</span>
+							</dd>
+						</div>
+						<div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+							<dt className="text-sm font-medium text-gray-500">Email Addresse</dt>
+							<dd className="flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow">timo.imhof@dlrg.de</span>
+								<span className="flex-shrink-0 ml-4">
+									<button
+										type="button"
+										className="font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Update
+									</button>
+								</span>
+							</dd>
+						</div>
+						<div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+							<dt className="text-sm font-medium text-gray-500">Datum</dt>
+							<dd className="flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow"> 01.01.1970 </span>
+								<span className="flex-shrink-0 ml-4">
+									<button
+										type="button"
+										className="font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Update
+									</button>
+								</span>
+							</dd>
+						</div>
+						<div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+							<dt className="text-sm font-medium text-gray-500">Beschreibung</dt>
+							<dd className="flex mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow">
+									Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+									qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure
+									nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+								</span>
+								<span className="flex-shrink-0 ml-4">
+									<button
+										type="button"
+										className="font-medium text-indigo-600 bg-white rounded-md hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										Update
+									</button>
+								</span>
+							</dd>
+						</div>
+					</dl>
+				</div>
+			</>
+
+
+			{/* Teilnehmer Section Heading */}
+			<div className="pb-5 mt-6 mb-3 border-b border-gray-200">
+				<h3 className="text-lg font-medium leading-6 text-gray-900">Teilnehmer</h3>
+			</div>
+
+
+			{/* TeilnehmerListe */}
+			<ul role="list" className="divide-y divide-gray-200">
+				{people.map((person) => (
+					<li key={person.email} className="flex py-4">
+						<img className="w-10 h-10 rounded-full" src={person.image} alt="" />
+						<div className="ml-3">
+							<p className="text-sm font-medium text-gray-900">{person.name}</p>
+							<p className="text-sm text-gray-500">{person.email}</p>
+						</div>
+					</li>
+				))}
+			</ul>
 
 			{/* Floating Button */}
 			<button
--- pages/organizer.tsx
+++ pages/organizer.tsx
@@ -204,27 +204,52 @@ const Organizer = () => {
 											<div className="mt-2">
 												{/* Neuen Kurs anlegen Dialog */}
 												<>
-													
+													<form className="space-y-8 divide-y divide-gray-200">
+														<div className="space-y-8 divide-y divide-gray-200">
+															
+															<div className="pt-8">
+																
+																<div className="grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6">
+																	
+																	<div className="sm:col-span-4">
+																		<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+																			Kursname
+																		</label>
+																		<div className="mt-1">
+																			<input
+																				id="email"
+																				name="email"
+																				type="text"
+																				autoComplete="off"
+																				className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+																			/>
+																		</div>
+																	</div>
+
+																</div>
+															</div>
+														</div>
+
+														<div className="pt-5">
+															<div className="flex justify-end">
+																<button
+																	type="button"
+																	className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+																>
+																	Cancel
+																</button>
+																<button
+																	type="submit"
+																	className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+																>
+																	Save
+																</button>
+															</div>
+														</div>
+													</form>
 												</>
 											</div>
 										</div>
-									</div>
-									<div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-										<button
-											type="button"
-											className="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
-											onClick={() => setOpen(false)}
-										>
-											Teile QR-Code
-										</button>
-										<button
-											type="button"
-											className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
-											onClick={() => setOpen(false)}
-											ref={cancelButtonRef}
-										>
-											Cancel
-										</button>
 									</div>
 								</Dialog.Panel>
 							</Transition.Child>
