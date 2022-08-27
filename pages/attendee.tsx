2a5a025e84efecc2f653ed85dfa88f006e969fac
Adjeko <adjeko88@gmail.com>
2022-08-27 10:47:10 +0200

    created attendee Page and organizer Page

---
+++ pages/attendee.tsx
@@ -0,0 +1,12 @@
+import AppShell from "../components/appshell"
+
+
+const Attendee = () => {
+    return (
+        <AppShell>
+        	<h1> Fortbildungsliste</h1>
+        </AppShell>
+      )
+}
+
+export default Attendee
---
+++ pages/organizer.tsx
@@ -0,0 +1,12 @@
+import AppShell from "../components/appshell"
+
+
+const Organizer = () => {
+    return (
+        <AppShell>
+          <h1> Liste für Veranstalter</h1>
+        </AppShell>
+      )
+}
+
+export default Organizer
