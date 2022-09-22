0452201d7cfbefa94fcd0f9cb4352cfee3a11864
Adjeko <adjeko88@gmail.com>
2022-09-22 16:11:48 +0200

    add events from dialog

--- pages/organizer.tsx
+++ pages/organizer.tsx
@@ -105,11 +105,24 @@ const Organizer = () => {
 	const appwriteDatabase = useDLRGStore((state) => state.appDatabase);
 	const [events, SetEvents] = useState<Models.Document[]>();
 
+	const [addEventTitle, SetAddEventTitle] = useState("");
+
 	function closeDialog() {
 		setOpen(false);
 	}
 	function openDialog() {
 		setOpen(true);
+	}
+
+	function onDialogCancel() {
+		closeDialog();
+	}
+
+	function onDialogSave(e) {
+		e.preventDefault();
+		const promise = appwriteDatabase.createDocument('632c679ca801b391db60', "unique()", {title: addEventTitle})
+
+		closeDialog();
 	}
 
 	useEffect(() => {
@@ -187,7 +200,7 @@ const Organizer = () => {
 				<PlusIconOutline className="w-8 h-8" aria-hidden="true" />
 			</button>
 
-			{/* QR Code Dialog */}
+			{/* Course Add Dialog */}
 			<Transition.Root show={open} as={Fragment}>
 				<Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
 					<Transition.Child
@@ -224,11 +237,8 @@ const Organizer = () => {
 												<>
 													<form className="space-y-8 divide-y divide-gray-200">
 														<div className="space-y-8 divide-y divide-gray-200">
-
 															<div className="pt-8">
-
 																<div className="grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6">
-
 																	<div className="sm:col-span-4">
 																		<label htmlFor="email" className="block text-sm font-medium text-gray-700">
 																			Kursname
@@ -240,10 +250,10 @@ const Organizer = () => {
 																				type="text"
 																				autoComplete="off"
 																				className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+																				onChange={(e) => SetAddEventTitle(e.target.value)}
 																			/>
 																		</div>
 																	</div>
-
 																</div>
 															</div>
 														</div>
@@ -253,12 +263,14 @@ const Organizer = () => {
 																<button
 																	type="button"
 																	className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+																	onClick={onDialogCancel}
 																>
 																	Cancel
 																</button>
 																<button
 																	type="submit"
 																	className="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+																	onClick={onDialogSave}
 																>
 																	Save
 																</button>
