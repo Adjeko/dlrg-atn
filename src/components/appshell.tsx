229c5830efe9150a7bb78ee16422c2ef6325c2b0
Adjeko <adjeko88@gmail.com>
2022-11-22 20:26:08 +0100

    deleted copyright text

--- src/components/appshell.tsx
+++ src/components/appshell.tsx
@@ -303,8 +303,8 @@ const AppShell = (props: any) => {
         <footer>
           <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-8 lg:max-w-7xl">
             <div className="py-8 text-sm text-center text-gray-500 border-t border-gray-200 sm:text-left">
-              <span className="block sm:inline">&copy; 2021 Tailwind Labs Inc.</span>{' '}
-              <span className="block sm:inline">All rights reserved.</span>
+              <span className="block sm:inline"></span>{' '}
+              <span className="block sm:inline"></span>
             </div>
           </div>
         </footer>
