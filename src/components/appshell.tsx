f6e3207d4dca26e4e68d3aa7f435c2f6165cdc89
Adjeko <adjeko88@gmail.com>
2023-02-12 14:40:55 +0100

    pass event to onClick

--- src/components/appshell.tsx
+++ src/components/appshell.tsx
@@ -88,7 +88,7 @@ const AppShell = (props: any) => {
                                     active ? 'bg-gray-100' : '',
                                     'block px-4 py-2 text-sm text-gray-700'
                                   )}
-                                  onClick={() => item.onClick()}
+                                  onClick={(e) => item.onClick(e)}
                                 >
                                   {item.name}
                                 </a>
