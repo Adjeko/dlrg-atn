891281c6b903080a268d5e1aa4e1782aabeb7479
Adjeko <adjeko88@gmail.com>
2023-09-25 20:41:55 +0200

    changed profile button the use user data

--- src/app/(Layout)/profile.tsx
+++ src/app/(Layout)/profile.tsx
@@ -1,15 +1,19 @@
 // 'use server'
 
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-import PocketBase from 'pocketbase';
+import PocketBase, { AuthModel } from 'pocketbase';
 import Cookies from 'js-cookie';
+import { getCurrentUser } from '@/services/pocketbase';
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Profile() {
+
+  const [user, setUser] = useState<AuthModel>();
+  const [userImageUrl, setUserImageUrl] = useState<string>(`https://ui-avatars.com/api/?name=Unknown?background=random`)
 
   const userNavigation = [
     // { name: 'Your Profile', href: '#', onClick: ()=>{}},
@@ -24,7 +28,15 @@ export default function Profile() {
       Cookies.remove("pb_auth")
     }
   }
-  const userImageUrl = `https://ui-avatars.com/api/?name=Herbert Pietrzyk?background=random`
+  
+
+  useEffect(() => {
+    (async () => {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser)
+      setUserImageUrl(`https://ui-avatars.com/api/?name=${currentUser?.name}?background=random`)
+    })();
+  }, [])
 
   return (
     <>
@@ -45,7 +57,7 @@ export default function Profile() {
           >
             <Menu.Items className="absolute z-40 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg -right-2 ring-black ring-opacity-5 focus:outline-none">
               <Menu.Item>
-                <p className='block px-4 py-2 text-sm text-gray-700'>Herbert Pietrzyk</p>
+                <p className='block px-4 py-2 text-sm text-gray-700'>{user?.name}</p>
               </Menu.Item>
               {userNavigation.map((item) => (
                 <Menu.Item key={item.name}>
