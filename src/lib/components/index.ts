72302389df8e438ab94063f9e52613ce9faf96ea
Adjeko <adjeko88@gmail.com>
2024-02-22 20:04:05 +0100

    corrected errors

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,4 @@
 import Input from './Input.svelte';
 import Modal from './Modal.svelte';
-import MyProjectItem from './MyProjectItem.svelte';
 import TextArea  from './TextArea.svelte';
-export { Input, Modal, MyProjectItem, TextArea};
+export { Input, Modal, TextArea};
