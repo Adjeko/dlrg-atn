ed2e293fba79306ebdddd9357a17eea08fcd754b
Adjeko <adjeko88@gmail.com>
2024-02-22 14:19:53 +0100

    added timeline with data

---
+++ src/routes/timeline/+page.server.ts
@@ -0,0 +1,27 @@
+import { error, redirect } from '@sveltejs/kit';
+
+
+export async function load ({ locals } : any) {
+	if (!locals.pb.authStore.isValid) {
+		throw redirect(303, '/login');
+	}
+
+	const getJoinedCourses = async (userId : any) => {
+		try {
+			const projects = 
+				await locals.pb.collection('isMemberOf').getFullList(undefined, {
+					filter: `user = "t78b293a3r4hrvz"`,
+          expand: `course`
+				})
+			
+			return projects;
+		} catch (err : any) {
+			console.log('Error: ', err);
+			throw error(err.status, err.message);
+		}
+	};
+
+	return {
+		courses: await getJoinedCourses(locals.user.id)
+	};
+};
---
+++ src/routes/timeline/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let data : any;
+</script>
+
+
+<h2 class="text-3xl font-bold">Timeline</h2>
+<div class="w-full mt-4 flex flex-col items-center">
+	{#if data.courses.length === 0}
+		<p class="text-center text-3xl">☹️</p>
+		<p class="text-center text-3xl">Looks like you don't have any projects.</p>
+		<a href="/projects/new" class="btn btn-primary max-w-md mt-4">Add One</a>
+	{:else}
+		{#each data.courses as course}
+			<p>{course.expand.course.name} - {course.expand.course.startDate}</p>
+			<div class="divider mt-0 mb-2" />
+		{/each}
+	{/if}
+</div>
+
