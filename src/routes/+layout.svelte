779d366dc4aa2c31a311d21eb8f01ccd267b81c4
Adjeko <adjeko88@gmail.com>
2024-02-27 16:07:52 +0100

    removed login and register buttons from navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,12 +16,6 @@
     <div class="flex-none">
       {#if !data.user}
         <div class="dropdown dropdown-end">
-          <a href="/login" class="btn btn-primary">
-            Login
-          </a>
-          <a href="/register" class="btn btn-secondary">
-            Register
-          </a>
         </div>
       {:else}
         <div class="dropdown dropdown-end mr-4">
