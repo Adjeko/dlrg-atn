da7114edd8ddcdb55dd2c99489c11e798cfb6b16
Adjeko <adjeko88@gmail.com>
2025-07-29 19:08:08 +0200

    corrected Date in PDF

--- src/lib/pdfGeneration.ts
+++ src/lib/pdfGeneration.ts
@@ -11,7 +11,7 @@ export function generateStyledPDF(user : User, start : Date, end : Date,schedule
      doc.setFontSize(20);
      doc.text("Ausbildungs- und Tätigkeitsnachweis", 50, 16);
      doc.setFontSize(12);
-     doc.text(`von ${start.toLocaleString('de-DE', { year: 'numeric', month: 'long'})} bis ${end.toLocaleString('de-DE', { year: 'numeric', month: 'long'})}`, 50, 21);
+     doc.text(`von ${start.toLocaleString('de-DE', { month: 'long'})}. ${start.toLocaleString('de-DE', { year: 'numeric'})} bis ${end.toLocaleString('de-DE', { month: 'long'})}. ${end.toLocaleString('de-DE', { year: 'numeric'})}`, 50, 21);
 
     // Briefkopf
     doc.addImage(logo, "PNG", 10, 10, 30, 30); // Logo hinzufügen
