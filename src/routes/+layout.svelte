f2aa769b9890d509c18d92fc2b2de4b9c021a4fe
Adjeko <adjeko88@gmail.com>
2025-07-30 18:50:21 +0200

    fixed account info in icon on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -141,8 +141,8 @@
 				<DropdownMenuContent class="w-56" align="end">
 					<DropdownMenuLabel class="font-normal">
 						<div class="flex flex-col space-y-1">
-							<p class="text-sm font-medium leading-none">John Doe</p>
-							<p class="text-xs leading-none text-muted-foreground">john@example.com</p>
+							<p class="text-sm font-medium leading-none">{user?.name}</p>
+							<p class="text-xs leading-none text-muted-foreground">{user?.email}</p>
 						</div>
 					</DropdownMenuLabel>
 					<DropdownMenuSeparator />
