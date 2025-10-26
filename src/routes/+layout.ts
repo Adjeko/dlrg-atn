24416df71e093aca3427c59daa0c9cf85dcc70fc
Adjeko <adjeko88@gmail.com>
2025-10-26 15:28:56 +0100

    redirect / to /timeline

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,13 @@ import { PB } from "@/lib/stores/pocketbase.svelte";
 export const ssr = false;
 
 export const load = async ({ url }) => {
+
+    if (url.pathname === '/') {
+        // Clientseitige Weiterleitung
+        goto('/timeline');
+        return;
+    }
+
     const publicRoutes = ['/login', '/register']; // Erlaubte öffentliche Routen
     const isPublicRoute = publicRoutes.includes(url.pathname);   
 
@@ -12,7 +19,6 @@ export const load = async ({ url }) => {
     if (!isPublicRoute && !PB.instance.authStore.isValid) {
         // Clientseitige Weiterleitung
         goto('/login');
-    }
-
-    return;
+        return;
+    }    
 };
